040eb7ed5149394ed1badef7d8c6b5e4d75ce435
Willian Souza <84147549+KyanityTnT@users.noreply.github.com>
2021-06-01 17:59:25 -0300

    Dia 2 Insider

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,14 +1,26 @@
-import React from 'react';
+import React, {useState} from 'react';
+import { TouchableWithoutFeedback, Keyboard, KeyboardAvoidingView, Platform, Modal } from 'react-native';
 
 
 import{ LinearGradient} from 'expo-linear-gradient';
 import StatusBarPage from '../../components/StatusBarPage';
 import Menu from '../../components/Menu';
+import ModalLink from '../../components/ModalLink';
 import{Feather} from '@expo/vector-icons';
 import { ContainerLogo, Logo, ContainerContent, Title, SubTitle, ContainerInput, BoxIcon, Input, ButtonLink, ButtonLinkText } from './styles';
 
 export default function Home(){
+
+    const [input, setInput] = useState('');
+    const [modalVisible, setModalVisible] = useState(false);
+
+    function handleShortLink(){
+        //alert('URL Encurtada: '+input )
+        setModalVisible(true);
+    }
+
     return(
+        <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
         <LinearGradient
         colors={['#E5E5E5','#132742' ]}
         style={{ flex:1, justifyContent: 'center'}}
@@ -24,6 +36,11 @@ export default function Home(){
             />
 
             <Menu/>
+            <KeyboardAvoidingView 
+            behavior={ Platform.OS === 'android' ? 'padding' : 'position'}
+            enabled
+
+            >
 
             <ContainerLogo>
                 <Logo source={require('../../assests/Logo.png')}>
@@ -53,19 +70,31 @@ export default function Home(){
                 <Input
                 placeholder="Cole seu link aqui..."
                 placeholderTextColor="white"
+                outCapitaLize="none"
+                outCorrect={false}
+                keyboardType="url"
+                value={input}
+                onChangeText={ (text) => setInput(text) }
                 
                 />
 
 
             </ContainerInput>
 
-            <ButtonLink>
+            <ButtonLink onPress={ handleShortLink} >
                 <ButtonLinkText>Gerar Link</ButtonLinkText>
 
             </ButtonLink>
 
            </ContainerContent>
+           </KeyboardAvoidingView>
+
+        <Modal visible={modalVisible} transparent animationType='slide'>
+        <ModalLink onClose={()=> setModalVisible(false)}/>
+
+        </Modal>
 
         </LinearGradient>
+        </TouchableWithoutFeedback>
     )
 }
